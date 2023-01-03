9a0f98ef5bc4aa4ede1943d647f9f0c72838645a
Anton Lapshin <anton@lapshin.dev>
2023-01-03 23:50:12 +0200

    Add fetching users and displaying it

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
   def index
-    user = User.find_by(id: session[:user_id])
-    redirect_to new_login_path unless user
+    @user = User.find_by(id: session[:user_id])
+    @users = User.eager_load(:posts).all
+    #redirect_to new_login_path unless @user
   end
 end
