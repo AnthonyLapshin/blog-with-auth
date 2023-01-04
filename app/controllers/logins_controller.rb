ce8673557a402fef049a57834b8351ae1b00c34a
Anton Lapshin <anton@lapshin.dev>
2023-01-05 00:38:44 +0200

    Finished task

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,7 +1,7 @@
 class LoginsController < ApplicationController
   def new
     @user = User.find_by(id: session[:user_id])
-    redirect_to dashboards_path if @user
+    redirect_to posts_path if @user
     @user = User.new
   end
 
@@ -9,11 +9,16 @@ class LoginsController < ApplicationController
     @user = User.find_by login_params
     if @user
       session[:user_id] = @user.id
-      redirect_to dashboards_url
+      redirect_to posts_url
     else
       flash[:alert] = "Incorrect email or password!"
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def logout
+    reset_session
+    redirect_to posts_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :dashboards, only: %i[index]
+  resources :posts
+  #resources :dashboards, only: %i[index]
   resources :registrations, only: %i[new create]
   resources :logins, only: %i[new create]
+  get "logout", to: "logins#logout"
 end
